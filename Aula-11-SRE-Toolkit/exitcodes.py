be8b616c1fd6219086f9284ba08ecea4db1cfd3b
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2026-03-06 23:04:34 -0300

    Add additional exit codes to classification function

--- Aula-11-SRE-Toolkit/exitcodes.py
+++ Aula-11-SRE-Toolkit/exitcodes.py
@@ -2,7 +2,10 @@ def classificar_exit_code(code):
     mapa_codigo = {
         200: ("Saudável", "ok"),
         500: ("Erro Interno", "critical"),
-        404: ("Não Encontrado", "warning")
+        404: ("Não Encontrado", "warning"),
+        301: ("Redirecionamento", "info"),
+        403: ("Proibido", "warning"),
+        503: ("Serviço indisponível", "critical")
     }
 
     return mapa_codigo.get(code, ("Código desconhecido", "unknown"))
